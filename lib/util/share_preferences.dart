8d739f5848b61b2559d3075e5b977824bac20677
BigtoC <taitocdt@gmail.com>
2020-06-03 17:22:01 +0800

    Reduce duplicated codes for enhancing code structure

--- lib/util/share_preferences.dart
+++ lib/util/share_preferences.dart
@@ -3,11 +3,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 // Set data in shared preferences
 void setDataInSharedPref(String key, String val) async {
   SharedPreferences sharedPref = await SharedPreferences.getInstance();
+
+  while (null == sharedPref) {
+    await new Future.delayed(new Duration(milliseconds: 100));
+  }
+
   sharedPref.setString(key, val);
 }
 
 // Get data from shared preferences
 Future<String> getDataFromSharedPref(String key) async {
   SharedPreferences sharedPref = await SharedPreferences.getInstance();
-  return sharedPref.getString(key);
+  var rtnVal = sharedPref.getString(key);
+
+  while (null == rtnVal) {
+    await new Future.delayed(new Duration(milliseconds: 100));
+  }
+
+  return rtnVal;
 }
