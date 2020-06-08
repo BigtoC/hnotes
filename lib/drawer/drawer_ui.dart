744425b875f98546662815483a0c962d6ded9c65
BigtoC <taitocdt@gmail.com>
2020-06-08 23:30:53 +0800

    Modify version number

--- lib/drawer/drawer_ui.dart
+++ lib/drawer/drawer_ui.dart
@@ -130,7 +130,7 @@ Widget version() {
       alignment: Alignment.bottomLeft,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Text('\n  v1.5.2 \n'),
+        child: Text('\n  v1.6.1 \n'),
       ),
     ),
   );
