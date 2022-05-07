cdfafff4f3ea014e24daf6f81d52080281ba9a09
Bigto Chan <taitocdt@gmail.com>
2022-05-07 18:33:30 +0800

    TASK-3_0: Use PNG as background

--- lib/presentation/count_day/count_day_background.dart
+++ lib/presentation/count_day/count_day_background.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 Widget countDayBackground() {
   return Center(
     child: new Image.asset(
-      'assets/Images/sun-bg.jpg',
+      'assets/Images/sun-bg.png',
       fit: BoxFit.fitHeight,
       height: 2000,
     ),
