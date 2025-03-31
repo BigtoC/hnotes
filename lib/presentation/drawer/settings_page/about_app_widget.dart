21eddab81f151a1cdae86c17e907d9fabb76fe6d
Bigto Chan <taitocdt@gmail.com>
2025-03-31 23:51:33 +0800

    Problem: Images are outdated (#31)

--- lib/presentation/drawer/settings_page/about_app_widget.dart
+++ lib/presentation/drawer/settings_page/about_app_widget.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:hnotes/domain/common_data.dart';
+import 'package:hnotes/domain/theme/theme_model.dart';
 import 'package:hnotes/presentation/components/browser.dart';
 import 'package:hnotes/presentation/components/build_card_widget.dart';
 import 'package:hnotes/application/blockchain_info/blockchain_info_bloc.dart';
+import 'package:hnotes/infrastructure/local_storage/theme/theme_repository.dart';
 
 class AboutAppWidget extends StatelessWidget {
   const AboutAppWidget({super.key});
@@ -11,120 +13,137 @@ class AboutAppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     blockchainInfoBloc.fetchNetworkData();
+    final themeRepository = ThemeRepository();
 
     final ButtonStyle style = ElevatedButton.styleFrom(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
     );
 
     void openGitHub() {
-      Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-        return Browser(title: "BigtoC/hnotes", url: "https://github.com/BigtoC/hnotes");
-      }));
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) {
+            return Browser(
+              title: "BigtoC/hnotes",
+              url: "https://github.com/BigtoC/hnotes",
+            );
+          },
+        ),
+      );
     }
 
     return buildCardWidget(
-        context,
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            cardTitle('About App'),
-            Container(
-              height: 40,
-            ),
-            cardContentTitle('Developed by'),
-            cardContent(context, 'Bigto Chan'),
-            Container(
-              alignment: Alignment.center,
-              child: OutlinedButton.icon(
-                icon: Icon(Icons.code),
-                label: Text('GITHUB',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        letterSpacing: 1,
-                        color: Colors.grey.shade500)),
-                style: style,
-                onPressed: openGitHub,
-              ),
-            ),
-            cardContentGap(),
-            cardContentTitle('Co-Designer'),
-            cardContent(context, 'Rita vv'),
-            cardContentGap(),
-            cardContentTitle('Made With'),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    FlutterLogo(
-                      size: 40,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Flutter',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    )
-                  ],
+      context,
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          cardTitle('About App'),
+          Container(height: 40),
+          cardContentTitle('Developer'),
+          cardContent(context, 'Bigto Chan'),
+          Container(
+            alignment: Alignment.center,
+            child: OutlinedButton.icon(
+              icon: Icon(Icons.code),
+              label: Text(
+                'GITHUB',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 1,
+                  color: Colors.grey.shade500,
                 ),
               ),
+              style: style,
+              onPressed: openGitHub,
             ),
-            Container(
-              height: 30,
-            ),
-            cardContentTitle('Blockchain Platform'),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset(
-                      "assets/Images/logo/Ethereum-Logo.png",
-                      height: 40,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: FutureBuilder(
-                        future: blockchainInfoBloc.fetchNetworkData(),
-                        builder: (context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
-                          String networkName = "......";
-                          if (snapshot.hasError) {
-                            logger.e(snapshot.error);
-                            return defaultNetwork(networkName);
-                          }
-                          if (snapshot.hasData) {
-                            networkName = snapshot.data!["text"].toString();
-                          }
-                          return defaultNetwork(networkName);
-                        },
-                      ),
-                    ),
-                  ],
-                ),
+          ),
+          cardContentGap(),
+          cardContentTitle('Co-Designer'),
+          cardContent(context, 'vv'),
+          cardContentGap(),
+          cardContentTitle('Framework'),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlutterLogo(size: 40),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Flutter', style: TextStyle(fontSize: 24)),
+                  ),
+                ],
               ),
             ),
-            Container(
-              alignment: Alignment.center,
-              child: Image.asset(
-                "assets/Images/logo/alchemy-logo-blue-gradient.png",
-                height: 40,
+          ),
+          Container(height: 30),
+          cardContentTitle('Blockchain'),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    "assets/Images/logo/MANTRA-Gradient.png",
+                    height: 40,
+                  ),
+                  // Padding(
+                  //   padding: const EdgeInsets.all(8.0),
+                  //   child: FutureBuilder(
+                  //     future: blockchainInfoBloc.fetchNetworkData(),
+                  //     builder: (context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+                  //       String networkName = "......";
+                  //       if (snapshot.hasError) {
+                  //         logger.e(snapshot.error);
+                  //         return defaultNetwork(networkName);
+                  //       }
+                  //       if (snapshot.hasData) {
+                  //         networkName = snapshot.data!["text"].toString();
+                  //       }
+                  //       return defaultNetwork(networkName);
+                  //     },
+                  //   ),
+                  // ),
+                ],
               ),
             ),
-            cardContentGap(),
-            cardContentTitle('Version'),
-            cardContent(context, "${packageInfo.version}+${packageInfo.buildNumber}"),
-            cardContentGap(),
-          ],
-        ));
+          ),
+          cardContentGap(),
+          Container(
+            alignment: Alignment.center,
+            child: FutureBuilder(
+              future: themeRepository.getSavedTheme(),
+              builder: (context, AsyncSnapshot<ThemeModel> snapshot) {
+                if (snapshot.hasError) {
+                  logger.e(snapshot.error);
+                  return Container();
+                }
+                if (snapshot.hasData) {
+                  final imagePath =
+                      snapshot.data!.brightness == Brightness.light
+                          ? "assets/Images/logo/CosmWasm-black.png"
+                          : "assets/Images/logo/CosmWasm-white.png";
+                  return Image.asset(imagePath, height: 40);
+                }
+                return Container();
+              },
+            ),
+          ),
+          cardContentGap(),
+          cardContentTitle('Version'),
+          cardContent(
+            context,
+            "${packageInfo.version}+${packageInfo.buildNumber}",
+          ),
+          cardContentGap(),
+        ],
+      ),
+    );
   }
 
   Widget defaultNetwork(String networkName) {
-    return Text(
-      "Ethereum \n$networkName",
-      style: TextStyle(fontSize: 22),
-    );
+    return Text("Ethereum \n$networkName", style: TextStyle(fontSize: 22));
   }
 }
