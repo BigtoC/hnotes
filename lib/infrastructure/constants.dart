5560a95cae3215a61eeea4f0e58822441b9b0a4c
BigtoC <taitocdt@gmail.com>
2025-04-07 01:14:27 +0800

    chore(code): Add constant

--- lib/infrastructure/constants.dart
+++ lib/infrastructure/constants.dart
@@ -1,3 +1,4 @@
 const String chainRestUrl = "https://api.dukong.mantrachain.io";
 const String chainRpcUrl = "https://rpc.dukong.mantrachain.io";
 const String chainWalletPrefix = "mantra";
+const String feeDenom = "uom";
