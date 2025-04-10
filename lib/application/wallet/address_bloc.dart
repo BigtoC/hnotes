c4aa19333e7ed75fad055a0a7156dbd690e2762c
BigtoC <taitocdt@gmail.com>
2025-04-10 23:46:34 +0800

    feat(wallet): Refactor wallet management and add WalletBloc

--- lib/application/wallet/address_bloc.dart
+++ lib/application/wallet/address_bloc.dart
@@ -1,60 +1,34 @@
-import "package:hnotes/infrastructure/blockchain/address_repository.dart";
-import "package:mantrachain_dart_sdk/api.dart";
 import "package:rxdart/rxdart.dart";
 
-import "package:hnotes/infrastructure/local_storage/secrets/secrets_repository.dart";
+import "package:hnotes/domain/blockchain/dtos/address_dto.dart";
+import "package:hnotes/infrastructure/blockchain/address_repository.dart";
 
 class AddressBloc {
-  final _secretsRepository = SecretsRepository();
   final _addressRepository = AddressRepository();
 
-  final _walletPrivateKey = PublishSubject<String>();
-  final _walletAddress = PublishSubject<String>();
   final _addressAndBalance = PublishSubject<Map<String, String>>();
-  final _walletBalances = PublishSubject<List<Coin>>();
-
-  Stream<String> get walletPrivateKeyStream => _walletPrivateKey.stream;
-
-  Stream<String> get walletAddressStream => _walletAddress.stream;
+  final _addressBalances = PublishSubject<List<CoinWithExponent>>();
 
   Stream<Map<String, String>> get addressAndBalanceStream =>
       _addressAndBalance.stream;
 
-  Stream<List<Coin>> get walletBalancesStream => _walletBalances.stream;
-
-  fetchSecret() async {
-    await getImportedWalletAddress();
-    await exportPrivateKey();
-  }
-
-  exportPrivateKey() async {
-    _walletPrivateKey.sink.add(await _secretsRepository.exportPrivateKey());
-  }
-
-  Future<String> importPrivateKey(String value) async {
-    return await _secretsRepository.deriveAndSaveWallet(value);
-  }
-
-  getImportedWalletAddress() async {
-    _walletAddress.sink.add(
-      await _secretsRepository.getImportedWalletAddress(),
-    );
-  }
+  Stream<List<CoinWithExponent>> get walletBalancesStream =>
+      _addressBalances.stream;
 
   getAddressAndBalance() async {
     final addressAndBalance = await _addressRepository.fetchAddressAndBalance();
     _addressAndBalance.sink.add(addressAndBalance);
   }
 
-  getWalletBalances(String address) async {
-    _walletBalances.sink.add(
+  getAddressBalances(String address) async {
+    _addressBalances.sink.add(
       await _addressRepository.fetchAddressBalances(address),
     );
   }
 
   void dispose() {
-    _walletPrivateKey.close();
-    _walletAddress.close();
+    _addressAndBalance.close();
+    _addressBalances.close();
   }
 }
 
--- lib/infrastructure/blockchain/address_repository.dart
+++ lib/infrastructure/blockchain/address_repository.dart
@@ -1,4 +1,5 @@
 import "dart:math";
+import "package:hnotes/domain/blockchain/dtos/address_dto.dart";
 import "package:mantrachain_dart_sdk/api.dart";
 
 import "package:hnotes/domain/common_data.dart";
@@ -16,12 +17,12 @@ class AddressRepository {
     }
     final balance = await queryApi.balance(address, denom: feeDenom);
     final metadata = await fetchSymbolMetadata(feeDenom);
-    final symbol = metadata["symbol"];
-    final exponent = metadata["exponent"];
+    final symbol = metadata.symbol;
+    final exponent = metadata.exponent;
     final amountRaw = balance?.balance?.amount ?? "0";
     final denomRaw = balance?.balance?.denom ?? feeDenom;
     final amount = (int.parse(amountRaw) / pow(10, exponent)).toStringAsFixed(
-      metadata["exponent"] ?? 0,
+      metadata.exponent,
     );
     return {
       "address": address,
@@ -30,11 +31,21 @@ class AddressRepository {
     };
   }
 
-  Future<List<Coin>> fetchAddressBalances(String address) async {
+  Future<List<CoinWithExponent>> fetchAddressBalances(String address) async {
     try {
       final balances = await queryApi.allBalances(address);
       if (balances != null) {
-        return balances.balances;
+        final metadataFutures =
+            balances.balances.map((coin) async {
+              final metadata = await fetchSymbolMetadata(coin.denom ?? "");
+              return CoinWithExponent(
+                denom: coin.denom ?? "",
+                amount: coin.amount ?? "0",
+                exponent: metadata.exponent,
+              );
+            }).toList();
+
+        return await Future.wait(metadataFutures);
       } else {
         return [];
       }
@@ -43,7 +54,7 @@ class AddressRepository {
     }
   }
 
-  fetchSymbolMetadata(String denom) async {
+  Future<SymbolMetadataDto> fetchSymbolMetadata(String denom) async {
     try {
       final metadata = await queryApi.denomMetadata(feeDenom);
       final display = metadata?.metadata?.display;
@@ -52,10 +63,15 @@ class AddressRepository {
       );
       final exponent = denomUnit?.exponent;
       final symbol = metadata?.metadata?.symbol;
-      return {"symbol": symbol, "exponent": exponent};
+
+      if (symbol == null || exponent == null) {
+        return SymbolMetadataDto(symbol: denom, exponent: 0);
+      }
+
+      return SymbolMetadataDto(symbol: symbol, exponent: exponent);
     } catch (e) {
       logger.e("Error fetching symbol metadata: $e");
-      return {"symbol": denom, "exponent": 0};
+      return SymbolMetadataDto(symbol: denom, exponent: 0);
     }
   }
 }
